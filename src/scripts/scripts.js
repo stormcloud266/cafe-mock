3e85ff8403a0350be233872e11319ed614d7c5f2
stormcloud266 <allprt89@gmail.com>
2018-08-24 10:50:56 -0600

    added nav scroll and menu toggles. fade in header

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,3 +1,21 @@
-$('h1').click(() => {
-    $('body').css('background', 'blue')
-})
+function menuToggle(toggled) {
+  $('.menu__nav ul li').removeClass('active');
+  $('.menu__section').removeClass('visible');
+  $('#' + toggled.target.classList[0]).addClass('visible');
+  $(toggled.target).addClass('active');
+};
+
+$('.menu__nav ul li').click((e) => {
+  menuToggle(e);
+});
+
+
+$(document).on('click', 'a[href^="#"]', function(e) {
+    const id = $(this).attr('href');
+    if ($(id).length === 0) {
+        return;
+    }
+    e.preventDefault();
+    const elTop = $(id).offset().top;
+    $('body, html').animate({scrollTop: elTop});
+});
